ebe33d88091d148262bda6bfd090169fd84f05c2
Abdoallah-Badr <Abdoallahbadr@outlook.com>
2024-01-21 10:37:25 +0200

    cleaning working tree

--- schoolmodule/models/classroom/classroom.py
+++
@@ -1,7 +0,0 @@
-from odoo import models,fields
-class Classroom(models.Model):
-    _name ='schoolmodule.classroom'
-    _description='classroom model'
-    name = fields.Char(string='Class ID',required=True)
-
-    student_ids=fields.One2many('schoolmodule.student','classroom_id',string='students')
